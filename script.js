51f985d8ad733b55da0fba9f7b41986d69680319
anissuletong <anissulala343@gmail.com>
2023-10-08 19:54:02 +0700

    if else

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// // example if...else, nested if
+// let nilai = 75;
+// if (nilai >= 90) {
+//   console.log("Nilai Anda A");
+// } else if (nilai >= 80) {
+//   console.log("Nilai Anda B");
+// } else if (nilai >= 70) {
+//   console.log("Nilai Anda C");
+// } else {
+//   console.log("Nilai Anda D");
+// }
+
 // example if...else, nested if
 let nilai = 75;
 if (nilai >= 90) {
