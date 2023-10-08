1cc70888a5183a12c18eb2a67f79dcd0a8cc46fb
anissuletong <anissulala343@gmail.com>
2023-10-08 22:04:56 +0700

    for statement

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-// // example if...else, nested if
-// let nilai = 75;
-// if (nilai >= 90) {
-//   console.log("Nilai Anda A");
-// } else if (nilai >= 80) {
-//   console.log("Nilai Anda B");
-// } else if (nilai >= 70) {
-//   console.log("Nilai Anda C");
-// } else {
-//   console.log("Nilai Anda D");
-// }
-
-// example if...else, nested if
-let nilai = 75;
-if (nilai >= 90) {
-  console.log("Nilai Anda A");
-} else if (nilai >= 80) {
-  console.log("Nilai Anda B");
-} else if (nilai >= 70) {
-  console.log("Nilai Anda C");
+// example if...else, 
+let buah = "mangga"; // Jenis buah buahan yang di pilih 
+if (buah === "apel") {
+  console.log("Ini adalah buah apel");
+} else if (buah === "pisang") {
+  console.log("Ini adalah buah pisang");
+} else if (buah === "mangga") {
+  console.log("Ini adalah buah mangga");
 } else {
-  console.log("Nilai Anda D");
+  console.log("Ini adalah buah lain");
 }
+
+
+// example nested if
+let tiket = 250; // Harga tiket
+if (tiket >= 200 && tiket < 300) {
+  console.log("Tiket agak mahal");
+  if (tiket >= 300) {
+    console.log("Tiket sangat mahal");
+  }
+} else if (tiket >= 300) {
+  console.log("Tiket sangat mahal");
+} else {
+  console.log("Tiket murah");
+}
+
 
 // example switch statement---
 let hari = "Kamis";
@@ -42,30 +45,37 @@ switch (hari) {
 }
 
 // example for statement
-for (let i = 1; i <= 5; i++) {
-    console.log("Iterasi ke-" + i);
+let jumlahGanjil = 0;
+
+for (var angka = 1; angka <= 10; angka++) {
+  if (angka % 2 !== 0) { // Memeriksa apakah angka ganjil
+    jumlahGanjil += angka;
   }
+}
+console.log("Jumlah bilangan ganjil dari 1 hingga 10 adalah: " + jumlahGanjil);
+
+
 
 // example while, do while
 let counter = 0;
-while (counter < 5) {
+while (counter < 3) {   //memeriksa kondisi sebelum menjalankan pernyataan di dalam loop
   console.log("While loop: " + counter);
   counter++;
 }
 
 let counter2 = 0;
-do {
+do {  //menjalankan pernyataan di dalam loop sekali sebelum memeriksa kondisi.
   console.log("Do-while loop: " + counter2);
   counter2++;
-} while (counter2 < 5);
+} while (counter2 < 3);
 
 // example funtion
 function kurang(a, b) {
-    return a - b;
+    return a - b;  //pernyataan yang ada dalam fungsi.ini mengembalikan hasil pengurangan a dan b sebagai hasil dari fungsi kurang.
   }
   
   let hasil = kurang(100, 50);
-  console.log("Hasil pengurangan: " + hasil);
+  console.log("Hasil pengurangan: " + hasil); //mencetak pesan Hasil pengurangan
   
 
 
